5834b53cd381d76e595b0f2b9bdd3eb3cfec2a97
Александр Русанов <shurik.82rusanov@yandex.ru>
2023-08-30 23:15:09 +0300

    add_models_viewsets_serializers

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Category, Genre, Title
+from reviews.models import Category, Genre, Title, TitleGenre
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -16,6 +16,28 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class TitleSerializer(serializers.ModelSerializer):
+    rating = serializers.SerializerMethodField()
+    genre = GenreSerializer(many=True)
+    category = CategorySerializer()
+
     class Meta:
         model = Title
-        fields = ('name', 'description', 'year', 'genre', 'category')
+        fields = ('id', 'name', 'description',
+                  'year', 'genre', 'rating', 'category')
+
+    def get_rating(self, obj):
+        return 0
+
+    def create(self, validated_data):
+        if 'genre' not in self.initial_data:
+            title = Title.objects.create(**validated_data)
+            return title
+
+        genres = validated_data.pop('genre')
+        title = Title.objects.create(**validated_data)
+        for genre in genres:
+            current_genre, status = Genre.objects.get_or_create(
+                **genre)
+            TitleGenre.objects.create(
+                genre=current_genre, title=title)
+        return title
