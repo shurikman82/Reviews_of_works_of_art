fd0395f5eb95baa41eb3621e5d08cdf75d79b033
Александр Русанов <shurik.82rusanov@yandex.ru>
2023-08-31 22:56:47 +0300

    add_search_pagination

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from reviews.models import Category, Genre, Title, TitleGenre
+from reviews.models import Category, Genre, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -17,8 +17,13 @@ class GenreSerializer(serializers.ModelSerializer):
 
 class TitleSerializer(serializers.ModelSerializer):
     rating = serializers.SerializerMethodField()
-    genre = GenreSerializer(many=True)
-    category = CategorySerializer()
+    genre = serializers.SlugRelatedField(
+        slug_field='name',
+        many=True,
+        queryset=Genre.objects.all()
+    )
+    category = serializers.SlugRelatedField(slug_field='name',
+                                            queryset=Category.objects.all())
 
     class Meta:
         model = Title
@@ -27,17 +32,3 @@ class TitleSerializer(serializers.ModelSerializer):
 
     def get_rating(self, obj):
         return 0
-
-    def create(self, validated_data):
-        if 'genre' not in self.initial_data:
-            title = Title.objects.create(**validated_data)
-            return title
-
-        genres = validated_data.pop('genre')
-        title = Title.objects.create(**validated_data)
-        for genre in genres:
-            current_genre, status = Genre.objects.get_or_create(
-                **genre)
-            TitleGenre.objects.create(
-                genre=current_genre, title=title)
-        return title
